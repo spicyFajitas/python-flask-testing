01172926f268c8521e99ed744243838b3f08a046
Adam F <68786858+afulton101222@users.noreply.github.com>
2022-09-16 10:17:12 -0400

    Development

--- dockerized/app.py
+++ dockerized/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page. <h1>HELLO!<h1>"
+    return "Hello! This is the main page for a dockerized python container. <h1>HELLO!<h1>"
 
 @app.route("/<name>")
 def user(name):
--- non-dockerized/test.py
+++ non-dockerized/test.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page. <h1>HELLO!<h1>"
+    return "Hello! This is the main page for the non-dockerized python application. <h1>HELLO!<h1>"
 
 @app.route("/<name>")
 def user(name):
